022e44b9b9046a6e0887a03e56e4e92a47b091f6
zylonity <kkhaleelkk505@gmail.com>
2025-11-15 21:14:45 +0000

    added todo comment

--- Craig_Vulkan/Craig/Craig_Renderer.cpp
+++ Craig_Vulkan/Craig/Craig_Renderer.cpp
@@ -1436,6 +1436,7 @@ void Craig::Renderer::updateUniformBuffer(uint32_t currentImage) {
 
     m_camera.m_aspect = m_VK_swapChainExtent.width / (float)m_VK_swapChainExtent.height;
     
+    //TODO: Figure this shit out
     ubo.model = glm::mat4(1.0f);//glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));// *glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     ubo.view = m_camera.getView();
     ubo.proj = m_camera.getProj();
