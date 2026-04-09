33b3ceed42eb33a47cd3e48584ccaa1f05f00576
zylonity <kkhaleelkk505@gmail.com>
2026-04-09 22:49:13 +0100

    Created the pipeline class files

---
+++ Craig_Vulkan/Craig/Renderer/Craig_Pipeline.cpp
@@ -0,0 +1,25 @@
+#include "Craig_Pipeline.hpp"
+
+CraigError Craig::Pipeline::init(const PipelineInitInfo& info) {
+
+	CraigError ret = CRAIG_SUCCESS;
+
+
+	return ret;
+}
+
+CraigError Craig::Pipeline::update() {
+
+	CraigError ret = CRAIG_SUCCESS;
+
+	return ret;
+}
+
+CraigError Craig::Pipeline::terminate() {
+
+	CraigError ret = CRAIG_SUCCESS;
+
+	return ret;
+}
+
+
